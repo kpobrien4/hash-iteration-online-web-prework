48c41691be442d55c5350e624b9e7b6493cd69b6
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-08 04:05:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@
    birthday_kids.each do |kids_name, age|
      if age < 12
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-   elsif 
+   elsif
      puts "You are too old for this"
    end
  end
