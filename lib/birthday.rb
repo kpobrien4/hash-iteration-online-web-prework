c2d4a0654effa656f3d9eb0898973b9342079d65
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-08 04:02:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@
  def happy_birthday(birthday_kids)
    birthday_kids.each do |kids_name, age|
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-     if age < 12
+     if |age| < 12
    end
  end
